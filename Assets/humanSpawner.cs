fd6178c6943d759aa8849878ddf5c46410903139
Mark <c16765279@mydit.ie>
2019-02-12 13:03:58 +0000

    human spawner

---
+++ Assets/humanSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class humanSpawner : MonoBehaviour
+{
+
+    public GameObject human;
+    public float spawnRate;
+    private float spawnTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnRate)
+        {
+            Instantiate(human, transform.position, Quaternion.identity);
+            spawnTimer = 0;
+        }
+    }
+}
