4b18891fce6eba0975293453a1365d0112cb1390
Mark <c16765279@mydit.ie>
2019-02-13 14:25:49 +0000

    randomise tree heigh + rotation

---
+++ Assets/treeRandomiser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treeRandomiser : MonoBehaviour
+    
+{
+    public float randomHeightModifier;
+    public float randomRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.eulerAngles = new Vector3(-90, transform.rotation.y, transform.rotation.z + (Random.Range(0, 360)));
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z + (Random.Range(-randomHeightModifier, randomHeightModifier)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
